d41a90ea65d3b8ef18c9231268135a2db778b344
7anvz <danjscommunity@gmail.com>
2025-11-17 09:10:32 -0500

    update

--- 01 CSHARP/02 PROBLEMAS_BASICO/02 ESTRUCTURAS_CONDICIONALES/16_exe.cs
+++ 01 CSHARP/02 PROBLEMAS_BASICO/02 ESTRUCTURAS_CONDICIONALES/16_exe.cs
@@ -4,20 +4,24 @@ public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        /*
+        /* 
         Pide tres números diferentes y muéstralos en orden ascendente.
+        PRIMERA FORMA
         */
         Console.Write("Escribe el 1er numero : ");
         double numero1 = double.Parse(Console.ReadLine());
-        Console.Write("Escribe el 1er numero : ");
+        Console.Write("Escribe el 2do numero : ");
         double numero2 = double.Parse(Console.ReadLine());
-        Console.Write("Escribe el 1er numero : ");
+        Console.Write("Escribe el 3er numero : ");
         double numero3 = double.Parse(Console.ReadLine());
         Console.WriteLine("Ordenándolos de forma ascendente...");
         Console.ReadKey();
-        if (numero1 < numero2 && numero2 < numero3) Console.WriteLine(string.Join(" - ", numero1, numero2, numero3));
+        if (numero1 < numero2 && numero2 < numero3) Console.WritLine(string.Join(" - ", numero1, numero2, numero3));
+        if (numero1 < numero3 && numero3 < numero2) Console.WritLine(string.Join(" - ", numero1, numero3, numero2));
+        else if (numero2 < numero1 && numero1 < numero3) Console.WriteLine(string.Join(" - ", numero2, numero1, numero3));
         else if (numero2 < numero3 && numero3 < numero1) Console.WriteLine(string.Join(" - ", numero2, numero3, numero1));
-        else if (numero3 < numero1 && numero1 < numero2) Console.WriteLine(string.Join(" - ", numero3, numero2, numero1));
-        else Console.WriteLine(string.Join(" - ", numero3, numero1, numero2));
+        else if (numero3 < numero2 && numero2 < numero1) Console.WriteLine(string.Join(" - ", numero3, numero2, numero1));
+        else if (numero3 < numero1 && numero1 < numero2) Console.WriteLine(string.Join(" - ", numero3, numero1, numero2));
+        else { Console.WriteLine("ERROR. AGAIN"); }
     }
 }
--- 01 CSHARP/02 PROBLEMAS_BASICO/02 ESTRUCTURAS_CONDICIONALES/17_exe.cs
+++ 01 CSHARP/02 PROBLEMAS_BASICO/02 ESTRUCTURAS_CONDICIONALES/17_exe.cs
@@ -5,8 +5,34 @@ public class HelloWorld
     public static void Main(string[] args)
     {
         /*
-
+        Pide tres números diferentes y muéstralos en orden ascendente.
+        SEGUNDA FORMA
         */
-        Console.Write("Hello World!");
+        Console.Write("Escribe el 1er numero");
+        double numero1 = double.Parse(Console.ReadLine());
+        Console.Write("Escribe el 2do numero");
+        double numero2 = double.Parse(Console.ReadLine());
+        Console.Write("Escribe el 3er numero");
+        double numero3 = double.Parse(Console.ReadLine());
+        double max, med, min;
+        if (numero1 < numero2 && numero1 < numero3)
+        {
+            min = numero1;
+            if (numero2 < numero3) { med = numero2; max = numero3; }
+            else { med = numero3; max = numero2; }
+        }
+        else if (numero2 < numero1 && numero2 < numero3)
+        {
+            min = numero2;
+            if (numero1 < numero3) { med = numero1; max = numero3; }
+            else { med = numero3; max = numero1; }
+        }
+        else
+        {
+            min = numero3;
+            if (numero1 < numero2) { med = numero1; max = numero2; }
+            else { med = numero2; max = numero1; }
+        }
+        Console.WriteLine(string.Join("|", min, med, max));
     }
 }
